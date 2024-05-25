97efeb4f76ced6461ba4183b75927254a2dec16c
kemothstudios <kemothstudioshosting@gmail.com>
2024-05-25 21:17:33 +0530

    Added fields to board configuration
    Added fields to board configuration

--- Assets/Scripts/ScriptableObjects/BoardConfigSO.cs
+++ Assets/Scripts/ScriptableObjects/BoardConfigSO.cs
@@ -4,6 +4,14 @@ namespace KemothStudios.Board
 {
     public class BoardConfigSO : ScriptableObject
     {
-        
+        [SerializeField, Min(1)] private int _rows = 1;
+        [SerializeField, Min(1)] private int _columns = 1;
+        [SerializeField, Min(0f)] private float _cellWidth = 1f;
+        [SerializeField, Min(0f)] private float _cellHeight = 1f;
+
+        public int rows => _rows;
+        public int columns => _columns;
+        public float cellWidth => _cellWidth;
+        public float cellHeight => _cellHeight;
     }
 }
